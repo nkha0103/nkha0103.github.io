f6123df26582f3d46cc2c3202fef4df8be51425f
Kha Nguyễn <nkha0103@gmail.com>
2019-08-29 14:29:01 +0700

    Initial commit

---
+++ js/my-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+//WOW
+
+new WOW().init();
+
+    // Typist 
+
+    $('.typist')
+        .typist({
+            speed: 12,
+            text: 'a Front End Developer'
+        })
+
+});
